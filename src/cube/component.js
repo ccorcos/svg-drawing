78bb66a3f355b2c55416376c2c6c296b45ce416f
Chet Corcos <chet.corcos@affirm.com>
2017-02-24 22:10:14 -0800

    closer

--- src/cube/component.js
+++ src/cube/component.js
@@ -1,23 +1,27 @@
+import { css } from 'glamor'
 import R from 'ramda'
+import { Vector3 } from 'three'
 
 const size = 200
 const half = size / 2
 const nhalf = -1 * half
 
 const styles = {
-  cube: {
+  cube: css({
     position: 'absolute',
     top: '50%',
     left: '50%',
-  },
-  face: {
+    perspective: '500px', // TODO another parameter
+    transformStyle: 'preserve-3d',
+  }),
+  face: css({
     borderStyle: 'solid',
     borderWidth: 1,
     borderColor: 'black',
     height: size,
     width: size,
     position: 'absolute',
-  }
+  }),
 }
 
 
@@ -35,78 +39,113 @@ const center = `translate3d(${nhalf}px, ${nhalf}px, 0)`
 const front = `translate3d(0px, 0px, ${half}px)`
 const back = `translate3d(0px, 0px, ${nhalf}px)`
 
-// CSS tranform rotation is in axis-angle notation.
-// "axis-angle between two 3d vectors"
+
 // http://www.euclideanspace.com/maths/algebra/vectors/angleBetween/
+// angle = acos(v1•v2)
+// axis = norm(v1 x v2)
 
-const dot = (v1, v2) =>
-  R.zip(v1, v2)
-  .map(([a, b]) => a * b)
-  .reduce((a, b) => a + b, 0)
-
+const getPoint = (e, n) => {
+  const box = n.getBoundingClientRect()
+  return {
+    x: (e.pageX - box.left) / size,
+    y: (e.pageY - box.top) / size,
+  }
+}
 
 const Cube = props => {
   const vec = props.state.get('vec')
-  const rotation = `rotate3d(1, 1, 1, 15deg)`
+  const unit = new Vector3(1, 0, 0)
+  const angle = Math.acos(vec.dot(unit))
+  const axis = vec.clone().cross(unit).normalize()
+  const rotation = `rotate3d(${axis.x}, ${axis.y}, ${axis.z}, ${angle}rad)`
+  let node
   return (
-    <div style={styles.cube}>
-      <div style={{
-        ...styles.face,
-        // center and z+
-        transform: [
-          center,
-          rotation,
-          front,
-        ].join(' '),
-      }}/>
-      <div style={{
-        ...styles.face,
-        // center and z-
-        transform: [
-          center,
-          rotation,
-          back,
-        ].join(' '),
-      }}/>
-      <div style={{
-        ...styles.face,
-        // center, spin y, and z+
-        transform: [
-          center,
-          rotation,
-          `rotate3d(0, 1, 0, 90deg)`,
-          front,
-        ].join(' '),
-      }}/>
-      <div style={{
-        ...styles.face,
-        // center, spin y, and z-
-        transform: [
-          center,
-          rotation,
-          `rotate3d(0, 1, 0, 90deg)`,
-          back,
-        ].join(' '),
-      }}/>
-      <div style={{
-        ...styles.face,
-        // center, spin x, and z+
-        transform: [
-          center,
-          rotation,
-          `rotate3d(1, 0, 0, 90deg)`,
-          front,
-        ].join(' '),
-      }}/>
-      <div style={{
-        ...styles.face,
-        // center, spin x, and z-
-        transform: [
-          center,
-          rotation,
-          `rotate3d(1, 0, 0, 90deg)`,
-          back,
-        ].join(' '),    }}/>
+    <div
+      className={styles.cube}
+      ref={n => node = n}
+      onMouseDown={e => props.onDragStart(getPoint(e, node))}
+      onTouchStart={e => props.onDragStart(getPoint(e, node))}
+      onMouseMove={e => props.state.get('dragging') && props.onDragMove(getPoint(e, node))}
+      onTouchMove={e => props.state.get('dragging') && props.onDragMove(getPoint(e, node))}
+      onMouseUp={e => props.state.get('dragging') && props.onDragEnd(getPoint(e, node))}
+      onTouchEnd={e => props.state.get('dragging') && props.onDragEnd(getPoint(e, node))}
+    >
+      <div
+        id="front"
+        style={{
+          transform: [
+            center,
+            rotation,
+            front,
+          ].join(' '),
+          backgroundColor: '#ffffff',
+        }}
+        className={styles.face}
+      />
+      <div
+        id="back"
+        className={styles.face}
+        style={{
+          transform: [
+            center,
+            rotation,
+            back,
+          ].join(' '),
+          backgroundColor: '#000000',
+        }}
+      />
+      <div
+        id="left"
+        className={styles.face}
+        style={{
+          transform: [
+            center,
+            rotation,
+            `rotate3d(0, 1, 0, 90deg)`,
+            front,
+          ].join(' '),
+          backgroundColor: '#fc9d9d',
+        }}
+      />
+      <div
+        id="right"
+        className={styles.face}
+        style={{
+          transform: [
+            center,
+            rotation,
+            `rotate3d(0, 1, 0, 90deg)`,
+            back,
+          ].join(' '),
+          backgroundColor: '#ef9efc',
+        }}
+      />
+      <div
+        id="bottom"
+        className={styles.face}
+        style={{
+          transform: [
+            center,
+            rotation,
+            `rotate3d(1, 0, 0, 90deg)`,
+            front,
+          ].join(' '),
+          backgroundColor: '#f8fc9e',
+        }}
+      />
+      <div
+        id="top"
+        className={styles.face}
+        style={{
+          transform: [
+            center,
+            rotation,
+            `rotate3d(1, 0, 0, 90deg)`,
+            back,
+          ].join(' '),
+          backgroundColor: '#b6fc9e',
+        }}
+      />
     </div>
   )
 }
--- src/cube/reducer.js
+++ src/cube/reducer.js
@@ -2,67 +2,54 @@ import Immutable from 'immutable'
 import actionTypes from './actionTypes'
 import { minDelta } from '../defs'
 import { distance, pipe } from '../utils'
+import { Vector3, Vector2 } from 'three'
 
 export const orientationInitialState = Immutable.Map({
-  vec: [1, 0, 0], // representing [x,y,z] orientation
+  vec: new Vector3(1, 0, 0),
   speed: 1, // rotation speed
   prev: undefined, // previous point
+  dragging: false,
 })
 
-// https://en.wikipedia.org/wiki/Rotation_matrix
-const rotateX = (vector, angle) => {
-  const [x, y, z] = vector
-  return [
-    x,
-    Math.cos(angle) * y - Math.sin(angle) * z,
-    Math.sin(angle) * y + Math.cos(angle) * z,
-  ]
-}
-
-const rotateY = (vector, angle) => {
-  const [x, y, z] = vector
-  return [
-    Math.cos(angle) * x + Math.sin(angle) * z,
-    y,
-    -1*Math.sin(angle) * x + Math.cos(angle) * z,
-  ]
-}
-
-const rotateZ = (vector, angle) => {
-  const [x, y, z] = vector
-  return [
-    Math.cos(angle) * x - Math.sin(angle) * y,
-    Math.sin(angle) * x + Math.cos(angle) * y,
-    z,
-  ]
-}
-
-const rotateToPoint = point => state => {
-  const [x0, y0] = state.get('prev')
-  const [x1, y1] = point
-  const dx = x1 - x0
-  const dy = y1 - y0
-  const vec = state.get('vec')
+const rotateToPoint = next => state => {
   const speed = state.get('speed')
-  const result = rotateY(rotateX(vec, dx * speed / 10), dy * speed / 10)
-  return state.set('vec', result)
+  const prev = state.get('prev')
+  const diff = next.clone().sub(prev)
+  const vec = state.get('vec')
+  // rotate 2D x-direction via y-axis
+  // rotate 2D y-direction via x-axis
+  console.log(diff.x, diff.y)
+  const newVec = vec.clone()
+    .applyAxisAngle(new Vector3(0, 1, 0), speed * diff.x * -2)
+    .applyAxisAngle(new Vector3(1, 0, 0), speed * diff.y * -2)
+  return state.set('vec', newVec).set('prev', next)
 }
+
+const setPoint = point => state => state.set('prev', point)
+const startDragging = state => state.set('dragging', true)
+const stopDragging = state => state.set('dragging', false)
 
 const orientationReducer = (state=orientationInitialState, action) => {
   switch(action.type) {
     case actionTypes.dragStart: {
-      return state.set('prev', action.point)
+      return pipe([
+        startDragging,
+        setPoint(new Vector2(action.point.x, action.point.y))
+      ], state)
     }
     case actionTypes.dragMove: {
+      const point = new Vector2(action.point.x, action.point.y)
       return pipe([
-        rotateToPoint(action.point),
-        s => s.set('prev', action.point)
+        rotateToPoint(point),
+        setPoint(point),
       ], state)
     }
     case actionTypes.dragEnd: {
+      const point = new Vector2(action.point.x, action.point.y)
       return pipe([
-        rotateToPoint(action.point),
-        s => s.set('prev', undefined)
+        stopDragging,
+        rotateToPoint(point),
+        setPoint(undefined),
       ], state)
     }
     default: {
